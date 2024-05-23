71a6d69be413b4fe9b8321ec629c147a3cc42e96
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-05-23 14:26:56 +0100

    chore: init calc microservice

--- apps/calc-service/src/main.ts
+++ apps/calc-service/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { CALCULATOR_PACKAGE_NAME } from './protos-ts/calc';
+import path from 'path';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('calc service started...');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: 'localhost:5001',
+        package: CALCULATOR_PACKAGE_NAME,
+        protoPath: path.resolve(__dirname, '../../../protos/calc.proto'),
+      },
+    },
+  );
+  await app.listen();
+  Logger.log('Calc microservice started...');
 }
 bootstrap();
